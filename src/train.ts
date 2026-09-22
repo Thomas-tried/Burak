670ff3f68deed2827cb97b45defbd22c7889bb47
Thomas-tried <jabbtemurbek@gmail.com>
2026-09-22 22:04:05 +0900

    feat: connect mongoDB via Mongoose

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,19 @@
 // TASK L
-
 // So'zlarni ketma-ketligini buzmasdan har bir so'zni alohida teskarisiga o'girib beradigan function tuzing.
-
 // Masalan: reverseSentence("we like coding!") return "ew ekil !gnidoc"
 
 // Masalani yechimi:
 
+// function reverseSentence(sentence: string) {
+//     return sentence.split(" ").map(gap => gap.split("").reverse().join("")).join(" ");
+// }
 
-function reverseSentence(sentence: string) {
-    return sentence.split(" ").map(gap => gap.split("").reverse().join("")).join(" ");
-}
+// let result = reverseSentence("Temur bugun dars qildimi?");
+// console.log(result);
 
-let result = reverseSentence("Temur bugun dars qildimi?");
-console.log(result);
+// TASK M
+// Array ichidagi har bir raqam uchun raqamning o'zi va uning kvadratidan tashkil topgan object hosil qilib qaytarsin.
+// Masalan: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, ...]
+
+// Masalani yechimi:
+
